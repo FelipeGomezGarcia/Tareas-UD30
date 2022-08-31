233539b91950cad0246ee94430086e1fcc0efd0e
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-31 20:45:45 +0200

    Mostrar factorial por el index

--- PR07/JS/codigo.js
+++ PR07/JS/codigo.js
@@ -4,4 +4,4 @@ var factorial = numero;
 for (let i = 1; i < numero; i++) {
     factorial *= i
 }
-document.getElementById("factorial").innerHTML("El factorial de " + numero + " es " + factorial);
+document.getElementById("factorial").innerHTML = "El factorial de " + numero + " es " + factorial;
