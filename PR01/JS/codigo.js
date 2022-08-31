e1f4a498ca42aba27fe8b936ed562cb1090e72ce
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-31 18:49:21 +0200

    Ejercicio 1

---
+++ PR01/JS/codigo.js
@@ -0,0 +1,6 @@
+//Es un script sencillo que imprime por la consola del navegador el texto entre parentesis
+console.log("Hola Mundo!");
+
+//Declaro una variable para demostrar que tambien funciona con variables
+var texto = "Soy el primer script";
+console.log(texto);
