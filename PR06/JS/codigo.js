ec41c205695c6d38ccb1d0165562b4f33539472a
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-31 21:32:32 +0200

    Correción de la array de letras

--- PR06/JS/codigo.js
+++ PR06/JS/codigo.js
@@ -1,4 +1,4 @@
-var letras = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','KK','E','T']
+var letras = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E','T']
 
 var dni = prompt("Pon tu dni","");
 var letra = dni.slice(-1);
