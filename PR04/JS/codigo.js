101c94a5e0d3d7c5344ee37fac0003b57fade316
FelipeGomezGarcia <feli.goga@gmail.com>
2022-09-01 09:30:48 +0200

    Operaciones de los booleanos

--- PR04/JS/codigo.js
+++ PR04/JS/codigo.js
@@ -1,30 +1,31 @@
 var valores = [true, 5, false, "hola", "adios", 2];
-var palabra;
-var numero;
-for (let index = 0; index < valores.length; index++) {
-    if (typeof(valores[index]) == "string" ) {
-        if (typeof(palabra) == "undefined") {
-            palabra = valores[index];
-        }else if (palabra.length > valores[index].length) {
-            console.log(palabra + " es más grande que " + valores[index])
-        }else{
-            console.log(palabra + " es más pequeña que " + valores[index])
-        }  
-    }
-    if (typeof(valores[index]) == "number" ) {
-        if (typeof(numero) == "undefined") {
-            numero = valores[index];
-        }else {
-            var resultado = numero + valores[index];
-            console.log(numero + " + " + valores[index] + " = " + resultado);
-            var resultado = numero - valores[index];
-            console.log(numero + " - " + valores[index] + " = " + resultado);
-            var resultado = numero * valores[index];
-            console.log(numero + " * " + valores[index] + " = " + resultado);
-            var resultado = numero / valores[index];
-            console.log(numero + " / " + valores[index] + " = " + resultado);
-            var resultado = numero % valores[index];
-            console.log(numero + " % " + valores[index] + " = " + resultado);
-        } 
-    }
+var palabra = valores.filter(valor => typeof(valor) == "string");
+var numero = valores.filter(valor => typeof(valor) == "number");
+var booleano = valores.filter(valor => typeof(valor) == "boolean")
+
+if (palabra[0].length > palabra[1].length) {
+    console.log(palabra[0] + " es más grande que " + palabra[1])
+}else{
+    console.log(palabra[0] + " es más pequeña que " + palabra[1])
 }
+
+var resultado = numero[0] + numero[1];
+console.log(numero[0] + " + " + numero[1] + " = " + resultado);
+var resultado = numero[0] - numero[1];
+console.log(numero[0] + " - " + numero[1] + " = " + resultado);
+var resultado = numero[0] * numero[1];
+console.log(numero[0] + " * " + numero[1] + " = " + resultado);
+var resultado = numero[0] / numero[1];
+console.log(numero[0] + " / " + numero[1] + " = " + resultado);
+var resultado = numero[0] % numero[1];
+console.log(numero[0] + " % " + numero[1] + " = " + resultado);
+
+console.log(booleano[0] + " && " + booleano[1] + " = " + (booleano[0]&&booleano[1]))
+console.log(booleano[0] + " && " + booleano[0] + " = " + (booleano[0]&&booleano[0]))
+console.log(booleano[1] + " && " + booleano[1] + " = " + (booleano[1]&&booleano[1]))
+console.log(booleano[1] + " && " + booleano[0] + " = " + (booleano[1]&&booleano[0]))
+
+console.log(booleano[0] + " || " + booleano[1] + " = " + (booleano[0]||booleano[1]))
+console.log(booleano[0] + " || " + booleano[0] + " = " + (booleano[0]||booleano[0]))
+console.log(booleano[1] + " || " + booleano[1] + " = " + (booleano[1]||booleano[1]))
+console.log(booleano[1] + " || " + booleano[0] + " = " + (booleano[1]||booleano[0]))
